2b362a22cbe080c82927e3a47067133c13fb5ba1
preettypeanuts <mrafly212001@gmail.com>
2024-07-23 14:02:03 +0700

    created database variable

---
+++ public/DB/OtherPackages.jsx
@@ -0,0 +1,360 @@
+export const SoftwarePackages = [
+    {
+        name: "Tipe Sistem",
+        products: [
+            {
+                name: "Accounting Software",
+                descriptions: "Accounting Software adalah perangkat lunak yang dirancang untuk membantu perusahaan dalam melakukan pencatatan keuangan, pengelolaan transaksi, dan penyusunan laporan keuangan. Ini menyediakan alat yang diperlukan untuk memudahkan proses akuntansi dan analisis keuangan."
+            },
+            {
+                name: "AR System",
+                descriptions: "AR System (Account Receivable System) adalah sistem yang mengelola dan memantau seluruh siklus piutang perusahaan. Ini mencakup pemantauan tagihan, pengelolaan pembayaran, dan penanganan situasi piutang agar keuangan perusahaan tetap sehat."
+            },
+            {
+                name: "PMS",
+                descriptions: "PMS (Property Management System) adalah sistem yang digunakan untuk mengelola properti dan aset perusahaan. Ini mencakup pemantauan inventaris, perawatan properti, dan pelacakan status aset untuk memastikan penggunaan yang efisien dan optimal."
+            }
+        ]
+    }
+];
+
+export const ManagementPackages = [
+    {
+        name: "Tipe Servis",
+        products: [
+            {
+                name: "Pelayanan Manajemen",
+                descriptions: "Pelayanan Manajemen adalah rangkaian solusi yang dirancang untuk membantu perusahaan dalam mengelola dan meningkatkan efisiensi operasional mereka. Ini mencakup berbagai layanan, mulai dari perencanaan strategis hingga implementasi praktik manajemen terbaik, untuk mencapai tujuan bisnis yang lebih baik."
+            }
+        ]
+    }
+];
+
+export const IzinPackages = [
+    {
+        name: "Izin Konstruksi dan Bangunan",
+        products: [
+            {
+                name: "SBUJK ( Sertifikat Badan Usaha Jasa Konstruksi )",
+                descriptions: "Izin yang diperlukan oleh perusahaan jasa konstruksi untuk menjalankan kegiatan usaha konstruksi, memastikan kualitas, dan memenuhi standar keamanan."
+            },
+            {
+                name: "SBUJPTL ( Sertifikat Badan Usaha Jasa Penunjang Tenaga Listrik )",
+                descriptions: "Izin untuk perusahaan jasa yang menyediakan layanan penunjang tenaga listrik, memastikan kompetensi dan keamanan dalam menyediakan tenaga listrik."
+            },
+            {
+                name: "SKK ( Sertifikat Kompetensi Kerja )",
+                descriptions: "Sertifikat yang menunjukkan bahwa individu atau perusahaan memiliki kompetensi dan keterampilan tertentu dalam melaksanakan pekerjaan konstruksi."
+            },
+            {
+                name: "IMB ( Izin Mendirikan Bangunan )",
+                descriptions: "Izin resmi yang diperlukan untuk memulai konstruksi bangunan. Dokumen ini menunjukkan kepatuhan proyek terhadap regulasi dan standar teknis yang berlaku, memastikan keamanan dan kelayakan."
+            },
+            {
+                name: "PBG ( Persetujuan Bangunan Gedung )",
+                descriptions: "izin resmi untuk memulai atau melanjutkan proyek pembangunan gedung, menunjukkan kepatuhan terhadap regulasi dan standar keamanan yang berlaku."
+            },
+            {
+                name: "SLF (Sertifikat Laik Fungsi)",
+                descriptions: "Dokumen yang menyatakan bahwa sebuah bangunan telah memenuhi persyaratan dan dapat digunakan sesuai fungsinya tanpa melanggar aturan atau standar yang berlaku."
+            }
+        ]
+    },
+    {
+        name: "Dokumentasi Desain dan Realisasi Bangunan",
+        products: [
+            {
+                name: "Site Plan",
+                descriptions: "Site Plan adalah gambaran grafis dari suatu area atau properti yang mencakup posisi dan tata letak elemen-elemen seperti bangunan, jalan, dan fasilitas lainnya. Biasanya digunakan sebagai panduan dalam perencanaan dan pengembangan suatu lokasi."
+            },
+            {
+                name: "GPA (Gambar Perencanaan Arsitektur)",
+                descriptions: "GPA atau Gambar Perencanaan Arsitektur adalah dokumentasi visual yang merinci desain dan perencanaan arsitektur suatu bangunan. Melibatkan detail tata letak, ukuran, dan spesifikasi konstruksi untuk memandu pelaksanaan proyek."
+            },
+            {
+                name: "As Built Drawing",
+                descriptions: "As Built Drawing adalah dokumentasi teknis yang mencerminkan kondisi aktual sebuah proyek konstruksi setelah selesai dibangun. Ini mencakup perubahan-perubahan atau modifikasi yang mungkin terjadi selama proses konstruksi dan memberikan pandangan akhir dari struktur atau instalasi yang telah dibangun."
+            }
+        ]
+    },
+    {
+        name: "Izin Transportasi",
+        products: [
+            {
+                name: "SIUPAL ( Surat Izin Usaha Perusahaan Angkutan Laut )",
+                descriptions: "Izin usaha yang diperlukan oleh perusahaan angkutan laut untuk menjalankan operasi angkutan laut secara legal dan aman."
+            },
+            {
+                name: "JPT ( Jasa Pengurusan Transportasi )",
+                descriptions: "Izin untuk perusahaan yang menyediakan jasa pengurusan transportasi, memastikan kepatuhan dengan regulasi dan standar operasional."
+            },
+            {
+                name: "Angkutan Multimoda",
+                descriptions: "Izin untuk perusahaan yang menyediakan layanan angkutan multimoda, mengintegrasikan berbagai mode transportasi untuk efisiensi dan keterhubungan yang lebih baik."
+            },
+            {
+                name: "Izin Usaha Tour & Travel",
+                descriptions: "Izin yang diperlukan oleh agen perjalanan untuk menyelenggarakan perjalanan wisata dan layanan perjalanan terkait."
+            },
+            {
+                name: "ANDALADIN ( Analisis Dampak Lalu Lintas )",
+                descriptions: "Analisis untuk menilai dampak lalu lintas dari proyek atau kegiatan tertentu terhadap sistem transportasi di sekitarnya."
+            },
+            {
+                name: "MRLL ( Manajemen dan Rekayasa Lalu Lintas )",
+                descriptions: "Izin untuk perusahaan yang menyediakan layanan manajemen dan rekayasa lalu lintas, memastikan kelancaran dan keamanan arus lalu lintas."
+            }
+        ]
+    },
+    {
+        name: "Izin Lingkungan Hidup",
+        products: [
+            {
+                name: "AMDAL ( Analisis Mengenai Dampak Lingkungan )",
+                descriptions: "Analisis untuk mengevaluasi dampak lingkungan dari proyek atau kegiatan tertentu, dengan tujuan mengidentifikasi dan mengelola risiko lingkungan."
+            },
+            {
+                name: "DELH ( Dokumen Evaluasi Lingkungan Hidup )",
+                descriptions: "Dokumen evaluasi yang menyajikan hasil analisis dampak lingkungan dan strategi pengelolaan dampak untuk mendukung keberlanjutan."
+            },
+            {
+                name: "UKL - UPL ( Upaya Pengelolaan dan Pemantauan Lingkungan Hidup )",
+                descriptions: "Izin dan upaya untuk mengelola dan memantau dampak lingkungan hidup sesuai dengan regulasi dan standar keberlanjutan."
+            },
+            {
+                name: "Laporan Implementasi Lingkungan Hidup ( Laporan Per-Semester)",
+                descriptions: "Laporan berkala yang mencatat implementasi dan kinerja perusahaan dalam menjaga keberlanjutan lingkungan hidup."
+            }
+        ]
+    },
+    {
+        name: "Izin Kelautan dan Daratan",
+        products: [
+            {
+                name: "IRK ( Izin Reklamasi Kelautan )",
+                descriptions: "Izin untuk melakukan reklamasi di wilayah laut, memungkinkan pengembangan lahan dari laut atau perairan."
+            },
+            {
+                name: "KRK ( Kajian Reklamasi Kelautan )",
+                descriptions: "Studi yang mengevaluasi dampak dan kelayakan reklamasi kelautan."
+            },
+            {
+                name: "KKPR ( Kajian Kelayakan Pemanfaatan Ruang )",
+                descriptions: "Studi yang menilai kelayakan pemanfaatan ruang daratan, mencakup evaluasi dampak lingkungan, ekonomi, dan sosial."
+            },
+            {
+                name: "IPR ( Izin Pemanfaatan Ruang )",
+                descriptions: "Izin yang memberikan hak legal untuk memanfaatkan atau mengembangkan suatu lahan atau wilayah tertentu."
+            }
+        ]
+    },
+    {
+        name: "Izin Usaha dan Kompetensi",
+        products: [
+            {
+                name: "SBUJK ( Sertifikat Badan Usaha Jasa Konstruksi )",
+                descriptions: "Izin yang menunjukkan bahwa badan usaha jasa konstruksi memiliki kompetensi dan kualifikasi untuk melakukan pekerjaan konstruksi."
+            },
+            {
+                name: "SBUJPTL ( Sertifikat Badan Usaha Jasa Penunjang Tenaga Listrik )",
+                descriptions: "Sertifikat yang menunjukkan bahwa badan usaha jasa penunjang tenaga listrik memiliki kualifikasi dan keahlian untuk menyediakan layanan tersebut."
+            },
+            {
+                name: "SKK ( Sertifikat Kompetensi Kerja )",
+                descriptions: "Sertifikat individu atau perusahaan yang menunjukkan tingkat kompetensi dalam melaksanakan pekerjaan tertentu."
+            },
+            {
+                name: "IPAK ( Izin Penyaluran Alat Kesehatan )",
+                descriptions: "Izin yang diperlukan untuk menyalurkan alat kesehatan ke pasaran, memastikan keamanan dan kualitas produk."
+            },
+            {
+                name: "SNI ( Sertifikasi N )",
+                descriptions: "Sertifikasi yang menunjukkan bahwa produk atau layanan memenuhi standar nasional tertentu."
+            },
+            {
+                name: "Pertek ( Persetujuan Teknis )",
+                descriptions: "Persetujuan teknis untuk memastikan bahwa proyek atau produk memenuhi standar teknis yang ditetapkan."
+            }
+        ]
+    },
+    {
+        name: "Izin Keamanan dan Operasional",
+        products: [
+            {
+                name: "SLO ( Sertifikat Laik Operasi )",
+                descriptions: "Sertifikat yang menunjukkan bahwa perusahaan atau fasilitas telah memenuhi persyaratan keamanan dan beroperasi sesuai dengan regulasi yang berlaku."
+            }
+        ]
+    }
+];
+
+export const HAKIPackages = [
+    {
+        name: "Tipe Servis",
+        products: [
+            {
+                name: "HAKI (Merek, Hak Cipta, Paten, Dll)",
+                descriptions: "HAKI (Hak Kekayaan Intelektual) meliputi merek, hak cipta, paten, dan aspek hukum lainnya yang melindungi kreativitas dan inovasi. Layanan ini membantu dalam pendaftaran, pengelolaan, dan perlindungan hak kekayaan intelektual perusahaan, memastikan keamanan hukum atas karya, inovasi, dan merek dagang."
+            }
+        ]
+    }
+];
+
+export const BUPackages = [
+    {
+        type: "Pendirian Yayasan",
+        price: 4000000,
+        priceOriginal: 6820000,
+        link: "https://api.whatsapp.com/send?phone=628887127000&text=Halo%20MinGans!%20Aku%20mau%20konsultasi%20terkait%20Pendirian%20Yayasan%20nih!%20",
+        discount: 36,
+        features: [
+            { feature: "Free Konsultasi", status: true },
+            { feature: "Pesan Nama Yayasan", status: true },
+            { feature: "Akta Pendirian Yayasan", status: true },
+            { feature: "Surat Keputusan Kemenkumham", status: true },
+            { feature: "NPWP Yayasan", status: true },
+            { feature: "Surat Keterangan Terdaftar Pajak Yayasan (SKT)", status: true },
+            { feature: "Nomor Induk Berusaha (NIB)", status: true },
+            { feature: "Pernyataan Mandiri OSS (K3L, Tata Ruang dan SPPL)", status: true },
+            { feature: "Sertifikat Standar (Jika resiko KBLI Menengah Rendah)", status: true },
+            { feature: "Hak Akses OSS", status: true },
+            { feature: "Free Email Yayasan", status: true },
+            { feature: "Free Stempel Nama Yayasan", status: true }
+        ]
+    },
+    {
+        type: "Pendirian Perkumpulan/Asosiasi",
+        price: 4000000,
+        priceOriginal: 6820000,
+        link: "https://api.whatsapp.com/send?phone=628887127000&text=Halo%20MinGans!%20Aku%20mau%20konsultasi%20terkait%20Pendirian%20Asosiasi/Perkumpulan%20nih!%20",
+        discount: 36,
+        features: [
+            { feature: "Free Konsultasi", status: true },
+            { feature: "Pesan Nama Perkumpulan", status: true },
+            { feature: "Akta Pendirian Yayasan", status: true },
+            { feature: "Surat Keputusan Kemenkumham", status: true },
+            { feature: "NPWP Yayasan", status: true },
+            { feature: "Surat Keterangan Terdaftar Pajak Yayasan (SKT)", status: true },
+            { feature: "Nomor Induk Berusaha (NIB)", status: true },
+            { feature: "Pernyataan Mandiri OSS (K3L, Tata Ruang dan SPPL)", status: true },
+            { feature: "Sertifikat Standar (Jika resiko KBLI Menengah Rendah)", status: true },
+            { feature: "Hak Akses OSS", status: true },
+            { feature: "Free Email Perkumpulan", status: true },
+            { feature: "Free Stempel Nama Perkumpulan", status: true }
+        ]
+    },
+    {
+        type: "Pendirian Firma Hukum",
+        price: 3000000,
+        priceOriginal: 4710000,
+        link: "https://api.whatsapp.com/send?phone=628887127000&text=Halo%20MinGans!%20Aku%20mau%20konsultasi%20terkait%20Pendirian%20Firma%20Hukum%20nih!%20",
+        discount: 36,
+        features: [
+            { feature: "Free Konsultasi", status: true },
+            { feature: "Pesan Nama Firma Hukum", status: true },
+            { feature: "Akta Pendirian Perkumpulan", status: true },
+            { feature: "Surat Keputusan Kemenkumham", status: true },
+            { feature: "NPWP Yayasan", status: true },
+            { feature: "Surat Keterangan Terdaftar Pajak Yayasan (SKT)", status: true },
+            { feature: "Nomor Induk Berusaha (NIB)", status: true },
+            { feature: "Pernyataan Mandiri OSS (K3L, Tata Ruang dan SPPL)", status: true },
+            { feature: "Sertifikat Standar (Jika resiko KBLI Menengah Rendah)", status: true },
+            { feature: "Hak Akses OSS", status: true },
+            { feature: "Free Email Firma Hukum", status: true },
+            { feature: "Free Stempel Firma Hukum", status: true }
+        ]
+    },
+    {
+        type: "Virtual Office",
+        price: 2652000,
+        priceOriginal: 4163640,
+        link: "https://api.whatsapp.com/send?phone=628887127000&text=Halo%20MinGans!%20Aku%20mau%20konsultasi%20terkait%20Pendirian%20Virtual%20Office%20nih!%20",
+        discount: 36,
+        features: [
+            { feature: "Perjanjian Sewa Menyewa 1 Tahun", status: true },
+            { feature: "Sudah Termasuk PPN dan PPH", status: true },
+            { feature: "Alamat Virtual Office Bisa PKP", status: true },
+            { feature: "Bisa Pakai Ruang Meeting diseluruh Cabang Virtual Office", status: true },
+            { feature: "Layanan Surat Menyurat", status: true },
+            { feature: "Notifikasi Atas Dokumen Atau Surat Masuk", status: true },
+            { feature: "Free Surat Domisili Gedung", status: true },
+            { feature: "Free Wifi", status: true },
+            { feature: "Self Service Pantry", status: true },
+            { feature: "Alamat Komersil dan Prestisius", status: true },
+            { feature: "Bonus Meeting Room Gratis 60-90 Jam Pertahun", status: true },
+            { feature: "Fasilitas Live Streaming", status: true },
+            { feature: "Fasilitas Podcast", status: true },
+            { feature: "Fasilitas Proyektor", status: true }
+        ]
+    },
+    {
+        type: "Pengurusan PKP (Pengusaha Kena Pajak)",
+        price: 1500000,
+        priceOriginal: 2500000,
+        link: "https://api.whatsapp.com/send?phone=628887127000&text=Halo%20MinGans!%20Aku%20mau%20konsultasi%20terkait%20Pengurusan%20PKP%20nih!%20",
+        discount: 40,
+        features: [
+            { feature: "Free Consultasi", status: true },
+            { feature: "SPPKP (Surat Pengukuhan Pengusaha Kena Pajak)", status: true },
+            { feature: "Sertifikat Elektronik (SE)", status: true }
+        ]
+    },
+    {
+        type: "Pengurusan NIB (Nomor Induk Berusaha)",
+        price: 500000,
+        priceOriginal: 1000000,
+        link: "https://api.whatsapp.com/send?phone=628887127000&text=Halo%20MinGans!%20Aku%20mau%20konsultasi%20terkait%20Pengurusan%20NIB%20nih!%20",
+        discount: 50,
+        features: [
+            { feature: "NIB (Nomor Induk Berusaha)", status: true },
+            { feature: "Pernyataan Mandiri (K3L, SPPL dan Tata Ruang)", status: true },
+            { feature: "Sertifiikat Standar (Jika Skala Resiko Menengah Rendah)", status: true },
+            { feature: "Hak Akses OSS", status: true }
+        ]
+    }
+];
+
+export const AuditPackages = [
+    {
+        name: "Tipe Audit",
+        products: [
+            {
+                name: "Audit External",
+                descriptions: "Audit External adalah jenis pemeriksaan keuangan yang dilakukan oleh pihak eksternal untuk mengevaluasi keuangan perusahaan. Pemeriksaan ini bertujuan untuk memastikan kepatuhan terhadap regulasi, mendeteksi potensi risiko, dan memberikan keyakinan kepada pemangku kepentingan eksternal."
+            },
+            {
+                name: "Audit Internal",
+                descriptions: "Audit Internal adalah proses pemeriksaan yang dilakukan oleh tim internal perusahaan untuk mengevaluasi efektivitas kontrol internal, memberikan saran perbaikan, dan memastikan bahwa operasi perusahaan berjalan sesuai dengan kebijakan dan prosedur yang telah ditetapkan."
+            }
+        ]
+    }
+];
+
+export const AccountantPackages = [
+    {
+        name: "Tipe Servis",
+        products: [
+            {
+                name: "Jurnal",
+                Descriptions: "Jurnal adalah catatan transaksi keuangan perusahaan dalam urutan kronologis. Jurnal mencatat semua transaksi, termasuk tanggal, jumlah, dan akun yang terpengaruh, untuk mempermudah penyusunan laporan keuangan dan analisis akuntansi."
+            },
+            {
+                name: "Buku Besar",
+                Descriptions: "Buku Besar adalah kumpulan semua akun yang digunakan oleh perusahaan, di mana setiap transaksi dicatat secara terperinci setelah dicatat di jurnal. Buku Besar membantu dalam memantau saldo akun dan mempersiapkan laporan keuangan yang akurat."
+            },
+            {
+                name: "Laporan Keuangan",
+                Descriptions: "Laporan Keuangan adalah dokumen yang menyajikan gambaran keuangan perusahaan selama periode tertentu. Ini mencakup Laporan Laba Rugi, Neraca, dan Laporan Arus Kas yang membantu manajemen dan pemangku kepentingan dalam membuat keputusan bisnis."
+            },
+            {
+                name: "Data Entry",
+                Descriptions: "Data Entry adalah proses memasukkan dan memproses data keuangan dari dokumen atau sumber lain ke dalam sistem akuntansi. Ini termasuk memasukkan transaksi ke dalam jurnal dan buku besar untuk memastikan akurasi dan kelengkapan data."
+            },
+            {
+                name: "Cost Control",
+                Descriptions: "Cost Control adalah proses memantau dan mengendalikan biaya perusahaan untuk memastikan bahwa pengeluaran tetap dalam anggaran dan efisien. Ini melibatkan analisis biaya, pembuatan anggaran, dan pengendalian biaya untuk meningkatkan profitabilitas."
+            }
+        ]
+    }
+];
+
