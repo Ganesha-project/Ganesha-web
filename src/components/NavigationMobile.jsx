1463c334175d3a495f180c2a875d895805a6a557
preettypeanuts <mrafly212001@gmail.com>
2024-09-19 19:29:27 +0700

    moreee detail on navigation

--- src/components/NavigationMobile.jsx
+++ src/components/NavigationMobile.jsx
@@ -6,26 +6,34 @@ import { usePathname } from "next/navigation";
 import { useEffect, useState } from "react";
 
 // pindahhin nanti
-import { RiAppsLine, RiArrowDropLeftLine, RiBookOpenLine, RiHomeLine, RiSearchLine } from "react-icons/ri";
+import { RiAppsFill, RiAppsLine, RiArrowDropLeftLine, RiBookOpenFill, RiBookOpenLine, RiHomeFill, RiHomeLine, RiSearchFill, RiSearchLine } from "react-icons/ri";
 
 export const NavigationMobile = () => {
     const [isMinimized, setIsMinimized] = useState(false);
-    const [isClicked, setIsClicked] = useState(false);
+    const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+    const toggleDropdown = () => {
+        setIsDropdownOpen(!isDropdownOpen);
+    };
+    
     const path = usePathname();
 
     const NavigationLinks = [
         {
             icon: <RiHomeLine />,
+            iconActive: <RiHomeFill />,
             label: "Home",
             link: "/"
         },
         {
             icon: <RiSearchLine />,
+            iconActive: <RiSearchFill />,
             label: "Search",
             link: "/services"
         },
         {
             icon: <RiBookOpenLine />,
+            iconActive: <RiBookOpenFill />,
             label: "Articles",
             link: "/article"
         },
@@ -63,10 +71,6 @@ export const NavigationMobile = () => {
         setIsMinimized(!isMinimized);
     };
 
-    const toggleClicked = () => {
-        setIsClicked(true);
-    };
-
     return (
         <>
             <section className="lg:hidden block">
@@ -79,12 +83,14 @@ export const NavigationMobile = () => {
                             {NavigationLinks.map((el, idx) => (
                                 <Link href={el.link} key={idx}>
                                     <button
-                                        className={`${(el.link === "/" && path === "/") ||
-                                            (el.link !== "/" && path.startsWith(el.link))
-                                            ? "bg-gray-300 dark:bg-darkColor px-3 py-[6px] rounded-full"
-                                            : "bg-opacity-0"
-                                            } ${(el.link === "/" && path === "/") ? "!-ml-7" : ""} text-xl flex flex-row items-center gap-2 opacity-60`}>
-                                        {el.icon}
+                                        className={`${(el.link === "/" && path === "/") || (el.link !== "/" && path.startsWith(el.link))
+                                            ? "bg-gray-300 dark:bg-darkColor px-3 py-[6px] rounded-full opacity-100"
+                                            : "bg-opacity-0 opacity-60"
+                                            } 
+                                            ${(el.link === "/" && path === "/") ? "!-ml-7" : ""} text-xl flex flex-row items-center gap-2`}
+                                    >
+                                        {(el.link === "/" && path === "/") || (el.link !== "/" && path.startsWith(el.link)) ? el.iconActive : el.icon}
+
                                         <span className={`${(el.link === "/" && path === "/") ||
                                             (el.link !== "/" && path.startsWith(el.link))
                                             ? "block font-semibold"
@@ -95,29 +101,28 @@ export const NavigationMobile = () => {
                                     </button>
                                 </Link>
                             ))}
-                            <div className="dropdown dropdown-top dropdown-end">
-                                <button
-                                    tabIndex={0}
-                                    className={`text-xl flex flex-row items-center gap-2 opacity-60 group`}>
-                                    <RiAppsLine className="duration-300 ease-in-out group-hover:drop-shadow-[0px_0px_5px_#aa0ab5] dark:group-hover:drop-shadow-[0px_0px_5px_#e600ff] group-hover:scale-[1.1] group-hover:animate-bounce" />
-                                </button>
-                                <ul tabIndex={0} className="mb-5 dropdown-content menu bg-gray-200 dark:bg-darkColor rounded-box z-[1] w-52 p-2 shadow">
-                                    {NavLinks.productsAndServices.map((el, id) => (
-                                        <li key={id}>
-                                            <a href={el.href}
-                                                className="flex items-center gap-2"
-                                            >
-                                                <span>
-                                                    {el.icon}
-                                                </span>
-                                                <span className="font-semibold">
-                                                    {el.label}
-                                                </span>
-                                            </a>
-                                        </li>
-                                    ))}
-                                </ul>
-                            </div>
+                    <div className="dropdown dropdown-top dropdown-end">
+  <label
+    tabIndex={0}
+    className="swap swap-rotate hover:swap-active">
+    <span className="swap-off">
+      <RiAppsLine className="text-xl duration-300 ease-in-out hover:drop-shadow-[0px_0px_5px_#aa0ab5] dark:hover:drop-shadow-[0px_0px_5px_#e600ff] hover:scale-[1.1] hover:animate-bounce" />
+    </span>
+    <span className="swap-on">
+      <RiAppsFill className="text-xl duration-300 ease-in-out hover:drop-shadow-[0px_0px_5px_#aa0ab5] dark:hover:drop-shadow-[0px_0px_5px_#e600ff] hover:scale-[1.1] hover:animate-bounce" />
+    </span>
+  </label>
+  <ul tabIndex={0} className="mb-5 dropdown-content menu bg-gray-200 dark:bg-darkColor rounded-box z-[1] w-52 p-2 shadow">
+    {NavLinks.productsAndServices.map((el, id) => (
+      <li key={id}>
+        <a href={el.href} className="flex items-center gap-2">
+          <span>{el.icon}</span>
+          <span className="font-semibold">{el.label}</span>
+        </a>
+      </li>
+    ))}
+  </ul>
+</div>
 
                             <button onClick={toggleMinimize} className={`${isMinimized ? "rotate-180 scale-0" : ""} bg-baseColor bg-opacity-50 h-fit -mr-7 p-1 rounded-full text-2xl flex flex-col items-center duration-300 ease-in-out`}>
                                 <RiArrowDropLeftLine />
