d0d6d752f3ab68aef85a19ff99a418329a64a48e
preettypeanuts <mrafly212001@gmail.com>
2024-09-19 14:48:43 +0700

    seo legalitas page

--- src/app/legalitas/page.js
+++ src/app/legalitas/page.js
@@ -8,15 +8,41 @@ import { DataPageLegal } from "../../../public/Data/LegalitasData";
 
 export const metadata = {
     title: 'Jasa Legalitas Bisnis - Ganesha Consulting',
-    description: 'Ganesha Consulting menyediakan jasa legalitas bisnis yang profesional untuk memastikan bisnis Anda sesuai dengan aturan hukum di Indonesia. Legalkan bisnis Anda sekarang bersama kami!',
-    keywords: 'jasa legalitas bisnis, konsultan legalitas, bisnis dan legalitas, legalitas usaha, konsultasi hukum',
-    canonical: 'https://www.ganeshaconsulting.co.id/all-products/legalitas',
-    ogTitle: 'Jasa Legalitas Bisnis - Ganesha Consulting',
-    ogDescription: 'Ganesha Consulting menyediakan jasa legalitas bisnis yang profesional untuk memastikan bisnis Anda sesuai dengan aturan hukum di Indonesia. Legalkan bisnis Anda sekarang bersama kami!',
-    twitterTitle: 'Jasa Legalitas Bisnis - Ganesha Consulting',
-    twitterDescription: 'Ganesha Consulting menyediakan jasa legalitas bisnis yang profesional untuk memastikan bisnis Anda sesuai dengan aturan hukum di Indonesia. Legalkan bisnis Anda sekarang bersama kami!',
+    description: 'Ganesha Consulting menyediakan jasa legalitas bisnis untuk membantu pendirian PT, CV, dan badan usaha lainnya. Kami siap membantu legalitas usaha Anda sesuai aturan hukum Indonesia.',
+    keywords: 'jasa legalitas bisnis, pendirian PT, pendirian CV, pendirian badan usaha, legalitas usaha, konsultasi hukum, Ganesha Consulting',
+    image: "https://res.cloudinary.com/dzrh2ogbn/image/upload/v1722236582/GaneshaAssets/ks0afkihiqamf7bjzgfi.png", // Path ke gambar banner
+    url: 'https://www.ganeshaconsulting.co.id/legalitas',
+    author: 'Ganesha Consulting',
+    siteName: 'Ganesha Consulting',
+    type: 'website',
+    robots: 'index, follow', // Aturan indeks dan pengikutan
+    locale: 'id_ID', // Lokalisasi halaman (misalnya, Indonesia)
+    // Open Graph metadata untuk berbagi di media sosial
+    openGraph: {
+        type: 'website',
+        url: 'https://www.ganeshaconsulting.co.id/legalitas',
+        title: 'Jasa Legalitas Bisnis - Ganesha Consulting',
+        description: 'Ganesha Consulting menyediakan jasa legalitas bisnis yang profesional untuk pendirian PT, CV, dan badan usaha lainnya. Legalkan bisnis Anda sekarang!',
+        image: {
+            url: 'https://res.cloudinary.com/dzrh2ogbn/image/upload/v1722236582/GaneshaAssets/ks0afkihiqamf7bjzgfi.png',
+            alt: 'Ganesha Consulting - Jasa Legalitas Bisnis'
+        }
+    },
+    // Structured data untuk membantu mesin pencari memahami konten halaman
+    structuredData: {
+        "@context": "http://schema.org",
+        "@type": "Organization",
+        name: "Ganesha Consulting",
+        url: "https://www.ganeshaconsulting.co.id/",
+        logo: "https://www.ganeshaconsulting.co.id/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FArtboard2.b33b65bb.png&w=96&q=75",
+        sameAs: [
+            "https://www.instagram.com/ganeshamultikreatif?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==",
+            "https://www.facebook.com/profile.php?id=61555739807086",
+            "https://www.linkedin.com/company/ganesha-multi-kreatif/",
+            "https://api.whatsapp.com/send?phone=628887127000&text=Halo%20Ganesha%20Consulting"
+        ]
+    }
 };
-
 
 export default function LegalitasPage() {
     return (
--- src/app/social-media-management/page.js
+++ src/app/social-media-management/page.js
@@ -14,8 +14,8 @@ export const metadata = {
     title: "Manajemen Sosial Media & Desain - Ganesha Consulting",
     description: "Ganesha Consulting menyediakan layanan manajemen sosial media dan desain untuk membantu bisnis Anda membangun citra merek yang kuat dan berinteraksi secara efektif dengan pelanggan. Temukan paket yang sesuai dengan kebutuhan Anda sekarang!",
     keywords: "manajemen sosial media, desain grafis, layanan sosial media, layanan desain, Ganesha Consulting",
-    image: "/BG/socmed.png", // Path ke gambar banner
-    url: "https://www.ganeshaconsulting.co.id/all-products/sosmed", // URL halaman Anda
+    image: "https://res.cloudinary.com/dzrh2ogbn/image/upload/v1722236583/GaneshaAssets/phln59clbvfecosy0msj.png", // Path ke gambar banner
+    url: "https://www.ganeshaconsulting.co.id/social-media-management", // URL halaman Anda
     author: "Ganesha Consulting",
     siteName: "Ganesha Consulting",
     type: "website",
@@ -24,19 +24,13 @@ export const metadata = {
     // Open Graph metadata untuk berbagi di media sosial
     openGraph: {
         type: "website",
-        url: "https://www.ganeshaconsulting.co.id/all-products/sosmed",
+        url: "https://www.ganeshaconsulting.co.id/social-media-management",
         title: "Manajemen Sosial Media & Desain - Ganesha Consulting",
         description: "Ganesha Consulting menyediakan layanan manajemen sosial media dan desain untuk membantu bisnis Anda membangun citra merek yang kuat dan berinteraksi secara efektif dengan pelanggan. Temukan paket yang sesuai dengan kebutuhan Anda sekarang!",
         image: {
-            url: "https://www.ganeshaconsulting.co.id/BG/socmed.png",
+            url: "https://res.cloudinary.com/dzrh2ogbn/image/upload/v1722236583/GaneshaAssets/phln59clbvfecosy0msj.png",
             alt: "Ganesha Consulting - Manajemen Sosial Media & Desain"
         }
-    },
-    // Twitter metadata untuk berbagi di Twitter
-    twitter: {
-        handle: "@ganeshaconsult",
-        site: "@ganeshaconsult",
-        cardType: "summary_large_image",
     },
     // Structured data untuk membantu mesin pencari memahami konten halaman
     structuredData: {
@@ -44,15 +38,15 @@ export const metadata = {
         "@type": "Organization",
         name: "Ganesha Consulting",
         url: "https://www.ganeshaconsulting.co.id/",
-        logo: "https://www.ganeshaconsulting.co.id/logo.png",
+        logo: "https://www.ganeshaconsulting.co.id/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FArtboard2.b33b65bb.png&w=96&q=75",
         sameAs: [
-            "https://www.facebook.com/ganeshaconsulting",
-            "https://twitter.com/ganeshaconsult",
-            "https://www.linkedin.com/company/ganeshaconsulting"
+            "https://www.instagram.com/ganeshamultikreatif?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==",
+            "https://www.facebook.com/profile.php?id=61555739807086",
+            "https://www.linkedin.com/company/ganesha-multi-kreatif/",
+            "https://api.whatsapp.com/send?phone=628887127000&text=Halo%20Ganesha%20Consulting"
         ]
     }
 };
-
 
 export default async function SosmedPage() {
     return (
@@ -66,7 +60,7 @@ export default async function SosmedPage() {
                 buttonColor={'bg-violet-500'}
                 label={'Paket Social Media Management'}
             />
-            <SocmedWork/>
+            <SocmedWork />
             <Header
                 data={dataPageSocmed.contentData}
             />
