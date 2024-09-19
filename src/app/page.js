0d5e46210b8cbf4ec81706a5d4d1b4657b5cb986
preettypeanuts <mrafly212001@gmail.com>
2024-09-19 14:27:05 +0700

    adding seo to home

--- src/app/page.js
+++ src/app/page.js
@@ -23,6 +23,43 @@ export default function Home() {
 
   return (
     <>
+      {/* SEO Meta Tags */}
+      <Head>
+        <title>Solusi Bisnis All In One - Ganesha Consulting</title>
+        <meta name="description" content="Ganesha Consulting menyediakan solusi all-in-one untuk bisnis Anda, termasuk pendirian PT atau CV, pengembangan website, manajemen media sosial, legalitas usaha, desain grafis, perpajakan, dan perizinan lainnya." />
+        <meta name="keywords" content="Ganesha Consulting, pendirian PT, pendirian CV, PT PMA, pajak, konsultan pajak, konsultan bisnis, solusi bisnis, pengembangan website, manajemen media sosial, legalitas usaha, desain grafis, perpajakan, perizinan, konsultasi IT, transformasi digital, pengembangan website, manajemen media sosial, Ganesha Consulting" />
+        <meta name="author" content="Ganesha Consulting" />
+        <meta property="og:title" content="Ganesha Consulting - Solusi Bisnis All-in-One" />
+        <meta property="og:description" content="Ganesha Consulting menyediakan solusi all-in-one untuk bisnis Anda, termasuk pendirian PT atau CV, pengembangan website, manajemen media sosial, legalitas usaha, desain grafis, perpajakan, dan perizinan lainnya." />
+        <meta property="og:url" content="https://www.ganeshaconsulting.co.id" />
+        <meta property="og:image" content="https://www.ganeshaconsulting.co.id/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FArtboard2.b33b65bb.png&w=96&q=75" />
+        <link rel="canonical" href="https://www.ganeshaconsulting.co.id" />
+        <script type="application/ld+json">
+          {`
+          {
+            "@context": "https://schema.org",
+            "@type": "ConsultingBusiness",
+            "name": "Ganesha Consulting",
+            "url": "https://www.ganeshaconsulting.co.id",
+            "logo": "https://www.ganeshaconsulting.co.id/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FArtboard2.b33b65bb.png&w=96&q=75",
+            "description": "Ganesha Consulting menyediakan solusi all-in-one untuk bisnis Anda, termasuk pendirian PT atau CV, pengembangan website, manajemen media sosial, legalitas usaha, desain grafis, perpajakan, dan perizinan lainnya.",
+            "address": {
+              "@type": "PostalAddress",
+              "streetAddress": "Jalan M.H. Thamrin",
+              "addressLocality": "Kota Adm. Jakarta Pusat",
+              "postalCode": "10340",
+              "addressCountry": "ID"
+            },
+            "contactPoint": {
+              "@type": "ContactPoint",
+              "telephone": "+628887127000",
+              "contactType": "Customer Service"
+            }
+          }
+          `}
+        </script>
+      </Head>
+
       <HomeBanner />
       <AllServicesBtn />
       <ClientPhotos />
