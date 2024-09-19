256d70bae4befc0a1e84b1f066f8da1d673a1de9
preettypeanuts <mrafly212001@gmail.com>
2024-09-19 15:41:28 +0700

    adding seo perizinan

--- src/app/perizinan/page.js
+++ src/app/perizinan/page.js
@@ -7,11 +7,11 @@ import { servicePackages } from '../../../public/DB/IzinPackages';
 import { ReusableCards } from '@/components/ReusableCards';
 
 export const metadata = {
-    title: "Izin Tambahan - Ganesha Consulting",
-    description: "Bantuan penuh pengalaman untuk memperoleh izin tambahan secara cepat dan efisien. Solusi mudah untuk semua kendala perizinan. Hubungi kami untuk konsultasi lebih lanjut!",
-    keywords: "izin tambahan, perizinan, bantuan izin, konsultasi perizinan, Ganesha Consulting",
-    image: "/BG/izin.jpg", // Path ke gambar terkait Izin Tambahan
-    url: "https://www.ganeshaconsulting.co.id/izin-tambahan", // URL halaman Izin Tambahan Anda
+    title: "Layanan Perizinan - Ganesha Consulting",
+    description: "Ganesha Consulting menyediakan layanan profesional untuk memperoleh Perizinan dengan cepat dan efisien. Temukan paket perizinan yang sesuai untuk kebutuhan Anda dan hubungi kami untuk konsultasi lebih lanjut.",
+    keywords: "Perizinan, perizinan, bantuan izin, konsultasi perizinan, Izin SKPL A, Pengurusan Izin SKPL A, Sertifikat SKPL A, SBUJK Skala Kecil, Pengurusan SBUJK Skala Kecil, Sertifikat Badan Usaha Jasa Konstruksi, KTA SBUJK Skala Kecil, SBUJK Skala Menengah, Pengurusan SBUJK Skala Menengah, Sertifikat Badan Usaha Jasa Konstruksi, KTA SBUJK Skala Menengah, SBUJK Skala Besar, Pengurusan SBUJK Skala Besar, Sertifikat Badan Usaha Jasa Konstruksi, KTA SBUJK Skala Besar, Sertifikat Kompetensi Kerja Jenjang 4-6, SKK Jenjang 4-6, Pengurusan SKK Jenjang 4-6, Kartu Tanda Anggota SKK, Sertifikat Kompetensi Kerja Jenjang 7, SKK Jenjang 7, Pengurusan SKK Jenjang 7, Kartu Tanda Anggota SKK, Sertifikat Kompetensi Kerja Jenjang 8-9, SKK Jenjang 8-9, Pengurusan SKK Jenjang 8-9, Kartu Tanda Anggota SKK, ISO 9001 Terakreditasi IAS/IAF, Sertifikat ISO 9001, Pengurusan ISO 9001, Sertifikasi ISO 9001, ISO 45001 Terakreditasi IAS/IAF, Sertifikat ISO 45001, Pengurusan ISO 45001, Sertifikasi ISO 45001, ISO 37001 Smap Akreditasi KAN, Sertifikat ISO 37001, Pengurusan ISO 37001, Sertifikasi ISO 37001 KAN, ISO 37001 Smap Akreditasi IAS/IAF, Sertifikat ISO 37001, Pengurusan ISO 37001, Sertifikasi ISO 37001 IAS/IAF, Audit Barcode Izin SBU, Audit Barcode SBU Skala Menengah, Audit Barcode SBU Skala Besar, Pengurusan Audit Barcode, Konsultasi Audit Barcode, Sistem Manajemen Keselamatan dan Kesehatan Kerja, SMK3, Pengurusan SMK3, Konsultasi SMK3, Sertifikat SMK3, Ganesha Consulting",
+    image: "https://res.cloudinary.com/dzrh2ogbn/image/upload/v1722236562/GaneshaAssets/lxcyp5lzimn5rv6fusir.svg", // Path ke gambar terkait Perizinan
+    url: "https://www.ganeshaconsulting.co.id/perizinan", // URL halaman Perizinan Anda
     author: "Ganesha Consulting",
     siteName: "Ganesha Consulting",
     type: "website",
@@ -20,19 +20,13 @@ export const metadata = {
     // Open Graph metadata untuk berbagi di media sosial
     openGraph: {
         type: "website",
-        url: "https://www.ganeshaconsulting.co.id/izin-tambahan",
-        title: "Izin Tambahan - Ganesha Consulting",
-        description: "Bantuan penuh pengalaman untuk memperoleh izin tambahan secara cepat dan efisien. Solusi mudah untuk semua kendala perizinan. Hubungi kami untuk konsultasi lebih lanjut!",
+        url: "https://www.ganeshaconsulting.co.id/perizinan",
+        title: "Layanan Perizinan - Ganesha Consulting",
+        description: "Ganesha Consulting menyediakan layanan profesional untuk memperoleh Perizinan dengan cepat dan efisien. Temukan paket perizinan yang sesuai untuk kebutuhan Anda dan hubungi kami untuk konsultasi lebih lanjut.",
         image: {
-            url: "https://www.ganeshaconsulting.co.id/BG/izin.jpg",
-            alt: "Ganesha Consulting - Izin Tambahan"
+            url: "https://res.cloudinary.com/dzrh2ogbn/image/upload/v1722236562/GaneshaAssets/lxcyp5lzimn5rv6fusir.svg",
+            alt: "Ganesha Consulting - Perizinan"
         }
-    },
-    // Twitter metadata untuk berbagi di Twitter
-    twitter: {
-        handle: "@ganeshaconsult",
-        site: "@ganeshaconsult",
-        cardType: "summary_large_image",
     },
     // Structured data untuk membantu mesin pencari memahami konten halaman
     structuredData: {
@@ -41,14 +35,25 @@ export const metadata = {
         name: "Ganesha Consulting",
         url: "https://www.ganeshaconsulting.co.id/",
         logo: "https://www.ganeshaconsulting.co.id/logo.png",
+        description: "Ganesha Consulting menyediakan layanan profesional untuk memperoleh Perizinan. Temukan berbagai paket perizinan dan hubungi kami untuk bantuan dalam proses perizinan Anda.",
         sameAs: [
-            "https://www.facebook.com/ganeshaconsulting",
-            "https://twitter.com/ganeshaconsult",
-            "https://www.linkedin.com/company/ganeshaconsulting"
-        ]
+            "https://www.instagram.com/ganeshamultikreatif?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==",
+            "https://www.facebook.com/profile.php?id=61555739807086",
+            "https://www.linkedin.com/company/ganesha-multi-kreatif/",
+            "https://api.whatsapp.com/send?phone=628887127000&text=Halo%20Ganesha%20Consulting"
+        ],
+        contactPoint: {
+            "@type": "ContactPoint",
+            telephone: "+628887127000",
+            contactType: "Customer Service"
+        },
+        address: {
+            "@type": "PostalAddress",
+            addressLocality: "Jakarta",
+            addressCountry: "ID"
+        }
     }
 };
-
 
 export default async function IzinTambahanPage() {
     return (
