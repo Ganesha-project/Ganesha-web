432b3714e4f2e7e10012dc145120fa6abbc621ca
preettypeanuts <mrafly212001@gmail.com>
2024-10-29 11:21:19 +0700

    add shadow to pse

--- src/components/PseCertificate.jsx
+++ src/components/PseCertificate.jsx
@@ -3,12 +3,12 @@ export const PseCertificate = () => {
         <>
             <section className="md:mx-24 mx-5 py-20">
                 <img
-                    className="w-full h-full object-cover rounded-3xl hidden md:inline-block"
+                    className="w-full h-full object-cover rounded-3xl hidden md:inline-block shadow-mainShadow"
                     src={'https://cms-ganesha.ganeshaconsulting.co.id/uploads/1_11zon_0a8babb962.webp'}
                     alt={'Ganesha Pse certificate'}
                 />
                 <img
-                    className="w-full h-full object-cover rounded-3xl inline-block md:hidden"
+                    className="w-full h-full object-cover rounded-3xl inline-block shadow-mainShadow md:hidden"
                     src={'https://cms-ganesha.ganeshaconsulting.co.id/uploads/2_11zon_1d4d53e8cf.webp'}
                     alt={'Ganesha Pse certificate'}
                 />
