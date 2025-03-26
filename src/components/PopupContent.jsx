6d7c3de23c28d7f4b001c483924387f173690f8c
preettypeanuts <mrafly212001@gmail.com>
2025-03-26 17:10:32 +0700

    add promo popup

--- src/components/PopupContent.jsx
+++ src/components/PopupContent.jsx
@@ -1,13 +1,13 @@
 export const PopupContent = () => {
     return (
         <>
-            <section className="w-[80lvw] h-[80lvw] md:w-[30lvw] md:h-[30lvw] carousel2">
-                {/* <img
+            <section className="w-[80lvw] h-[80lvw] md:w-[30lvw] md:h-[30lvw] carousel2 gap-2">
+                <img
                     width={100}
                     height={100}
                     className="w-[80lvw] h-[80lvw] md:w-[30lvw] md:h-[30lvw] object-cover rounded-2xl"
-                    src="https://cms-ganesha.ganeshaconsulting.co.id/uploads/DECEMBER_DEALS_38_OFF_GANESHA_9f3419f16a.webp"
-                    alt="Ganesha December Deals" /> */}
+                    src="https://cms-ganesha.ganeshaconsulting.co.id/uploads/Feed_2025_03_24_Ganesaha_Consulting_c7a6f61049.jpg"
+                    alt="Ganesha December Deals" />
                 <img
                     width={100}
                     height={100}
