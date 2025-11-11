5b7d2987d40006091677a2545e96204f805db9b5
VsalCode <vsal.ilham@gmail.com>
2025-11-11 11:24:19 +0700

    feat: add Meta Pixel integration for tracking page views and events

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,32 +6,61 @@ import { FloatingButton } from "@/components/FloatingButton";
 import { Analytics } from "@vercel/analytics/react";
 import { ThemeProvider } from "next-themes";
 import { NavigationMobile } from "@/components/NavigationMobile";
-import { SpeedInsights } from "@vercel/speed-insights/next"
-import Head from 'next/head';
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import Head from "next/head";
 import Script from "next/script";
-import { Popup } from "@/components/Popup";
-import { PopupContent } from "@/components/PopupContent";
 
 const quicksand = Plus_Jakarta_Sans({
   subsets: ["latin"],
-  weight: ["200", "300", "400", "500", "600", "700", "800"]
+  weight: ["200", "300", "400", "500", "600", "700", "800"],
 });
 
 export const metadata = {
   title: {
     default: "GaneshaConsulting",
-    template: '%s | GaneshaConsulting'
+    template: "%s | GaneshaConsulting",
   },
   description: "Legalkan Bisnis Mu Bersama Kami!",
 };
 
-export default function RootLayout({ children }) {
+export default function RootLayout({
+  children,
+}) {
   return (
     <html lang="en" suppressHydrationWarning className="scroll-smooth">
       <Head>
         <link rel="icon" type="image/png" sizes="16x16" href="icon.png" />
       </Head>
       <body className={`${quicksand.className}`}>
+        {/* ✅ META PIXEL BASE CODE */}
+        <Script
+          id="meta-pixel"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window,document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '2630106004010805');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=2630106004010805&ev=PageView&noscript=1"
+            alt="meta-pixel"
+          />
+        </noscript>
+
         {/* Google Tag Manager Script */}
         <Script
           async
@@ -56,17 +85,17 @@ export default function RootLayout({ children }) {
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8227813889502050"
           crossOrigin="anonymous"
         />
+
         <ThemeProvider enableSystem={true} attribute="class">
-            <Navbar>
-              {children}
-              <Footer />
-            </Navbar>
+          <Navbar>
+            {children}
+            <Footer />
+          </Navbar>
           <FloatingButton />
           <NavigationMobile />
-          {/* <Popup
-            content={<PopupContent />}
-          /> */}
+          {/* <Popup content={<PopupContent />} /> */}
         </ThemeProvider>
+
         <Analytics />
         <SpeedInsights />
       </body>
